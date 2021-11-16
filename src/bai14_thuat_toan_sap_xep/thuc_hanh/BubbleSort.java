1c8a1cc3c096d3d9e8e83d03fb50f2efcbd9e420
Wiz <trancongdat.2101@gmail.com>
2021-11-16 11:53:18 +0700

    nop bai 14

---
+++ src/bai14_thuat_toan_sap_xep/thuc_hanh/BubbleSort.java
@@ -0,0 +1,29 @@
+package bai14_thuat_toan_sap_xep.thuc_hanh;
+
+public class BubbleSort {
+    static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+
+    public static void bubbleSort(int[] list) {
+        boolean needNextPass = true;
+
+        for (int i = 1; i < list.length && needNextPass; i++) {
+            needNextPass = false;
+            for (int j = 0; j < list.length - i; j++) {
+                if (list[j] > list[j + 1]) {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for (int li : list) {
+            System.out.print(li + " ");
+        }
+    }
+}
